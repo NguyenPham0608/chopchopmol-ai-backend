cfeb7ca70e31e00f5e30d2272cb246b2d3dc24ef
Nguyen Pham <nguyenpham0806@gmail.com>
2025-12-10 17:14:29 -0800

    bug fixes

--- app.py
+++ app.py
@@ -555,7 +555,7 @@ def chat():
 
     try:
         response = client.chat.completions.create(
-            model="gpt-4.1",
+            model="gpt-5.1",
             messages=messages,
             tools=TOOLS,
             tool_choice="auto",
